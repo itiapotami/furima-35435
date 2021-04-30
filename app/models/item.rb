fd2588d3defb333fa5f208c7845986e3621662cb
itiapotami <yanagisawasoichi@gmail.com>
2021-04-30 17:50:33 +0900

    商品出品機能の実装

---
+++ app/models/item.rb
@@ -0,0 +1,34 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :fee
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :day
+
+  belongs_to       :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :description
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      format: { with: /\A[0-9]+\z/ }
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :day_id
+  end
+end
