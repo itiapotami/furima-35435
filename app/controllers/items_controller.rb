cc06c70f0a281ada2ccc18e391c20e8f9e5b827b
itiapotami <yanagisawasoichi@gmail.com>
2021-05-04 19:23:19 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.order(id: 'DESC')
@@ -20,6 +20,25 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])   
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+
+      redirect_to root_path
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.save
+      redirect_to root_path
+    else
+      @item = item
+      render :edit 
+    end
   end
 
   private
